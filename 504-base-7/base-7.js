17bd5a12ebdb0adc79d88d114fb774869a0b914c
ajithrajesh <ajithsriraj@gmail.com>
2026-03-13 14:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (60.12%) - LeetSync

---
+++ 504-base-7/base-7.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+
+    if (num === 0) return "0";
+
+    let negative = num < 0;
+    num = Math.abs(num);
+
+    let result = "";
+
+    while (num > 0) {
+        result = (num % 7) + result;
+        num = Math.floor(num / 7);
+    }
+
+    return negative ? "-" + result : result;
+};
