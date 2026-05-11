9f5bd8c84be7c1e56fa42959c7159f516ed80948
ajithrajesh <ajithsriraj@gmail.com>
2026-05-11 12:45:35 +0530

    Time: 2 ms (84.20%) | Memory: 60.4 MB (66.48%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.js
@@ -0,0 +1,23 @@
+var sortArrayByParityII = function(nums) {
+
+    let result = new Array(nums.length);
+
+    let even = 0;
+    let odd = 1;
+
+    for(let i = 0; i < nums.length; i++) {
+
+        if(nums[i] % 2 === 0) {
+
+            result[even] = nums[i];
+            even += 2;
+
+        } else {
+
+            result[odd] = nums[i];
+            odd += 2;
+        }
+    }
+
+    return result;
+};
