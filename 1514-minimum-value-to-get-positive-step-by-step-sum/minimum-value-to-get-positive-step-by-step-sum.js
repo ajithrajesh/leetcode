fe099a141e9533b28bebbfc18f5af1c3c5067dc4
ajithrajesh <ajithsriraj@gmail.com>
2026-05-23 15:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (99.20%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,17 @@
+function minStartValue(nums) {
+
+    let sum = 0;
+
+    let minSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        sum += nums[i];
+
+        minSum = Math.min(minSum, sum);
+
+    }
+
+    return 1 - minSum;
+
+}
