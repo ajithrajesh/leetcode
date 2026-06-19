051e4363bb97b1f40819b53a397e8698150b6549
ajithrajesh <ajithsriraj@gmail.com>
2026-06-19 11:57:17 +0530

    Time: 1 ms (50.07%) | Memory: 61.8 MB (43.73%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -0,0 +1,20 @@
+var removeElements = function(head, val) {
+
+    while (head !== null && head.val === val) {
+        head = head.next;
+    }
+
+    let current = head;
+
+    while (current !== null && current.next !== null) {
+
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+
+    }
+
+    return head;
+};
