8cb836546a0ba7db09a04b55ea1a70e783c61f58
ajithrajesh <ajithsriraj@gmail.com>
2026-08-20 17:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (86.03%) - LeetSync

---
+++ 67-add-binary/add-binary.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = "";
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let digitA = i >= 0 ? Number(a[i]) : 0;
+        let digitB = j >= 0 ? Number(b[j]) : 0;
+
+        let sum = digitA + digitB + carry;
+
+        result = (sum % 2) + result;
+        carry = Math.floor(sum / 2);
+
+        i--;
+        j--;
+    }
+
+    return result;
+};
